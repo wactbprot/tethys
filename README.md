6b619a85f6ff0f9d6e34be8174b0dee0cd105065
Thomas Bock <thomas.bock@ptb.de>
2022-08-24 15:21:14 +0200

    ini co

---
+++ README.md
@@ -0,0 +1,69 @@
+
+# design ideas
+
+## metis lessons learnd
+[metis](https://gitlab1.ptb.de/vaclab/metis) uses redis as the state
+image. An unpleasant side effect is the need of a constant `loc` (map)- `key`
+transformation (`map->key`, `key->map`).
+
+The hope that the redis database will be perceived
+as a platform for docking further programs (GUI, Metic, evaluation,
+data mining, alarm system, bots, ki) is wishful thinking. so why all
+the `map->key`, `key->map` time loss.
+
+Part of metis debuging was related to a redis gui with all it pros and
+cons. The _dxrt_ system should be inspectable in total with the clojure REPL.
+
+Furthermore, [portal](https://github.com/djblue/portal) is a realy
+nice option to understand the system during runtime.
+
+* no use of an (in-mem)-database at the first place
+* state, exchange, model **can be pulled into an inmutable (clock ordered) database system snapshots
+* a loop recur makes the progress (turn based) which simplifies a lot
+
+## system
+
+* the system is a collection of `mpd`s modeled as an image in a certain way
+* the system maintained  by [integrant](https://github.com/weavejester/integrant)
+
+## loc param (location map)
+
+* `loc` is a map describing the location of a mutable piece of information in the system
+* keys of this map are at least:
+  * `id`
+  * `group`
+  *  `ndx` `sdx` `pdx` 
+
+## task ns
+
+* task building in a namespace with (read only) access to database (CouchDB, or also clock ordered ?)
+* no state: get task always fresh from db and build it up on the fly like in previous system(s)
+* task ns should be rewritten
+
+## state ns
+
+* state is an agent since it is mutated async uncoord
+*  `(gen-state mpd config)` which means (same as in dx system):
+  `(assoc-in @image [:mpd-ref :container 0] (agent {:state [{:par 0 :seq 0 :state :ready} ...] :ctrl ready})`
+
+
+## exchange
+
+* exchange is an agent since it is mutated async uncoord
+* exchange is an agent `(assoc-in @image [:mpd-ref :exchange] (agent {:GetDefaultsFromMPD :all})`
+* worker got data for exchange: `(exchange/to data loc config)` which schould be `(send (image/exchange loc) data)`
+
+## document ns
+
+* for every document an agent `(assoc-in @image [:mpd-ref :ids :cal-foo] (agent {rev-1-bar})`
+* https://www.thattommyhall.com/2014/02/24/concurrency-and-parallelism-in-clojure/
+* https://stackoverflow.com/questions/4768592/use-of-agents-to-complete-side-effects-in-stm-transactions
+* worker got data to write to document: `(document/to data loc)` which schould be `(send (image/document loc) data)` location contains `:id :cal-foo`
+
+
+## worker ns
+
+* worker call: e.g. `(worker/wait task loc config)`
+* worker completed: `(state/executed loc)` which schould be `(send  (image/state loc) :executed)` where `(image/state loc)` is an agent
+  
+  
---
+++ src/tethys/system.clj
@@ -0,0 +1,114 @@
+(ns tethys.system
+  ^{:author "Thomas Bock <thomas.bock@ptb.de>"}
+  (:require [libcdb.core :as db]
+            [libcdb.configure :as cf]
+            [com.brunobonacci.mulog :as µ]
+            [integrant.core :as ig])
+  (:gen-class))
+
+
+(defn config [ids]
+  {:log/mulog {:type :multi
+               :log-context {:app-name "vl-db-agent"
+                             :facility (or (System/getenv "DEVPROXY_FACILITY")
+                                           (System/getenv "DEVHUB_FACILITY")
+                                           (System/getenv "METIS_FACILITY"))}
+               :publishers[{:type :elasticsearch
+                            :url  "http://a75438:9200/"
+                            :els-version  :v7.x
+                            :publish-delay 1000
+                            :data-stream  "vl-log-stream"
+                            :name-mangling false}]}
+   :db/couch {:prot "http",
+              :host "localhost",
+              :port 5984,
+              :usr (System/getenv "CAL_USR")
+              :pwd (System/getenv "CAL_PWD")
+              :name "vl_db_work"}
+   :db/mpds {:db (ig/ref :db/couch)
+             :ids ids
+             :ini {}}
+   :model/cont {:mpds (ig/ref :db/mpds)
+                :ids ids
+                :group-kw :cont
+                :ini {}}})
+
+;; # Database io functions
+;; The three functions used here are simply passed through from
+;; library [libcdb](https://gitlab1.ptb.de/vaclab/vl-db)
+(defn db-config [opts] (cf/config opts))
+(defn get-doc [id db] (db/get-doc id db))
+(defn put-doc [doc db] (db/put-doc doc db))
+
+(defn flattenv [v] (into [] (flatten v)))
+
+(defn agents-up [id group-kw m]
+  (mapv (fn [{:keys [Ctrl Definition]} ndx]
+          (agent {:ctrl (or (keyword Ctrl) :ready)
+                  :state (flattenv
+                          (mapv (fn [s sdx] 
+                                  (mapv (fn [t pdx] 
+                                          {:id id
+                                           :group group-kw
+                                           :ndx ndx :sdx sdx :pdx pdx
+                                           :is :ready
+                                           :task t})
+                                        s (range)))
+                                Definition (range)))})) 
+        m (range)))
+
+
+;; The first `if` clause (the happy path) contains the central idea:
+;; the request is send to
+;; an [agent](https://clojure.org/reference/agents) `a`. This queues
+;; up the write requests and avoids *write conflicts*
+
+;; # System
+;; The entire system is stored in an `atom` that is build up by the
+;; following `init-key` multimethods and shut down by `halt-key!`
+;; multimethods.
+;; ### System up multimethods
+(defonce system (atom nil))
+
+;; The `init-key`s methods **read a configuration** and **return an
+;; implementation**.
+(defmethod ig/init-key :log/mulog [_ opts]  
+  (µ/set-global-context! (:log-context opts))
+  (µ/start-publisher! opts))
+ 
+(defmethod ig/init-key :db/couch [_ opts]
+  (db-config opts))
+
+;; 
+(defmethod ig/init-key :db/mpds [_ {:keys [db ids ini]}]
+  (reduce
+   (fn [res id] (assoc res (keyword id) (:Mp (get-doc id db))))
+   ini ids))
+
+(defmethod ig/init-key :model/cont [_ {:keys [mpds ids ini group-kw]}]
+  (reduce
+   (fn [res [id mpd]] (assoc res id (agents-up id group-kw (:Container mpd) )))
+  ini mpds))
+
+
+;; ## Start, stop and restart The following functions are intended
+;; for [REPL](https://clojure.org/guides/repl/introduction) usage.
+(defn start []
+  (keys (reset! system (ig/init (config ["mpd-ppc-gas_dosing"
+                                         "mpd-se3-calib"])))))
+
+
+(defmethod ig/halt-key! :log/mulog [_ logger]
+  (logger))
+
+(defn stop []
+  (µ/log ::start :message "halt system")
+  (ig/halt! @system)
+  (reset! system {}))
+
+(defn restart []
+  (stop)
+  (Thread/sleep 1000)
+  (start))
+
+(defn mpds [] (keys (:model/cont @system)))
