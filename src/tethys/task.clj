4d56efaf0bc0f87a736a0d9b4534f234d289d1c2
Thomas Bock <thomas.bock@ptb.de>
2022-11-01 16:12:21 +0100

    assemble task fn

--- src/tethys/task.clj
+++ src/tethys/task.clj
@@ -2,6 +2,7 @@
   ^{:author "Thomas Bock <thomas.bock@ptb.de>"}
   (:require [com.brunobonacci.mulog :as µ]
             [clojure.data.json :as json]
+            [clojure.string :as string]
             [java-time.api :as jt]
             [tethys.db :as db]))
 
@@ -27,15 +28,31 @@
      "@day" (get-day d)
      "@time" (get-time)}))
 
+;; In order to overcome the problem of keywords like `:@foo` the
+;; function `kw-map->str-map` converts the "keyword map" delivered by
+;; the database view to a "string map".
+(defn kw-map->str-map [m] (-> m json/write-str json/read-str)) 
+
+(defn replace-map [s r]
+  (reduce (fn [res [k v]]
+            (string/replace res (re-pattern k) v))
+          s r))
 (comment
- (def Defaults "{\"@CR\":\"\\r\",\"@host\":\"e75550\",\"@port\":\"5303\",\"@acc\":\"TCP\",\"%safechannel%\":1}")
+ (def Defaults {"%CR%" "\r", "%host%" "e75551", "%port%" 503})
  (def task "{\"Port\":\"@port\",\"group\":\"cont\",\"ndx\":0,\"is\":\"ready\",\"TaskName\":\"PPC_DualGauge-ini\",\"Comment\":\"Initializes the safe gauge\",\"pdx\":0,\"sdx\":1,\"id\":\"mpd-ppc-gas_dosing\",\"Action\":\"@acc\",\"Defaults\":{\"@CR\":\"\\r\",\"@host\":\"e75550\",\"@port\":\"5303\",\"@acc\":\"TCP\",\"%safechannel%\":1},\"Value\":[\"UNI,0@CR\",\"\\u0005\",\"PR1@CR\",\"\\u0005\"],\"Host\":\"@host\"}")
  )
 
-(defn assemble [task Replace Use Defaults]
-  (prn (json/write-str Defaults))
-  (prn (json/write-str task))
-  task)
+(defn assemble [Replace Use Defaults FromExchange]
+  (let [str-task (json/write-str (dissoc task
+                                         :Replace
+                                         :Use
+                                         :Defaults
+                                         :FromExchange))]
+    (prn (-> str-task
+             (replace-map (kw-map->str-map Replace))
+             (replace-map (globals))
+             (replace-map (kw-map->str-map Defaults))))
+  task))
       
 ;; The `up` checks if the task vector contains any
 ;; elements. Returns m (the agent) if not. If `:task` is not empty it
@@ -45,7 +62,7 @@
         w (fn [_ a _ {:keys [task-queue work-queue] :as m}]
             (when (seq task-queue)
               (let [{:keys [TaskName Use Replace] :as task} (first task-queue)
-                    {:keys [Defaults] :as task} (merge task (f TaskName))
+                    {:keys [Defaults FromExchange] :as task} (merge task (f TaskName))
                     task (assemble task Replace Use Defaults)]
               (send a (fn [m] (assoc m
                                     :task-queue (-> task-queue
