3228cdc86dbd0536cb8e34d063fa80b58ce36680
Wact. B. Prot <wactbprot@gmail.com>
2022-11-13 13:41:57 +0100

    test check inspiration

--- test/tethys/task_test.clj
+++ test/tethys/task_test.clj
@@ -6,6 +6,10 @@
             [clojure.test.check.properties :as prop]))
 
 (comment
+  (def Defaults {"@CR" "\r", "@host" "e75551", "@port" 503})
+  (def task "{\"Port\":\"@port\",\"group\":\"cont\",\"ndx\":0,\"is\":\"ready\",\"TaskName\":\"PPC_DualGauge-ini\",\"Comment\":\"Initializes the safe gauge\",\"pdx\":0,\"sdx\":1,\"id\":\"mpd-ppc-gas_dosing\",\"Action\":\"@acc\",\"Value\":[\"UNI,0@CR\",\"\\u0005\",\"PR1@CR\",\"\\u0005\"],\"Host\":\"@host\"}"))
+
+(comment
   ;; https://clojure.org/guides/test_check_beginner
   (def sort-idempotent-prop
     (prop/for-all [v (gen/vector gen/int)]
@@ -15,4 +19,23 @@
 
 (comment
   (gen/sample gen/string)
-  ("" "" "¡" "" "" "O" "Av&Ç" "{" "ÿoý^\"$" "¬.V"))
+  ("" "" "¡" "" "" "O" "Av&Ç" "{" "ÿoý^\"$" "¬.V")
+
+(def replace-map-works
+  (prop/for-all [m (gen/map gen/string-ascii gen/string)]
+                (let [k (keys m)
+                      v (vals m)
+                      l (gen/sample  gen/string-ascii)
+                      r (gen/sample  gen/string-ascii)]
+                  (=  v (replace-map k  m)))))
+
+(tc/quick-check 10000 replace-map-works))
+
+
+(deftest error?-test-i
+  (testing "empty key"
+    (is (= "cacbc" (replace-map "ab" {"" "c"})))
+    (is (= "ab" (replace-map "ab" {"" ""}))))
+  (testing "empty val"
+    (is (= "" (replace-map "d" {"d" ""})))
+    (is (= "" (replace-map "" {"" ""})))))
